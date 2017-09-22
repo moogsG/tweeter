14d10fd4dca6f6cf30051bc6d92642d1ede8ca32
Morgan Greff <morgan@greffs.com>
2017-09-22 18:30:12 +0000

    More Server errors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Basic express setup:
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 require('dotenv').config();
 const express       = require("express");
 const bodyParser    = require("body-parser");
